d592d9fd8abcbcc47cf5ff919c846216b6604793
Amey Chintale <abchintale2003@gmail.com>
2026-02-17 23:22:28 +0530

    Fixing errors

--- backend/index.js
+++ backend/index.js
@@ -16,25 +16,22 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
-const corsOptions = {
-    origin:'http://localhost:5173',
-    credentials:true
-}
 
+const corsOptions = {
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173', 
+    credentials: true
+}
 app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
 
-
-// api's
+// apis
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
-
-
-app.listen(PORT,()=>{
-    connectDB();
+app.listen(PORT, async ()=>{
+    await connectDB(); 
     console.log(`Server running at port ${PORT}`);
 })
